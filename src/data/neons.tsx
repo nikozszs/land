d8ae9d349032337706ec5405e1414d3320e8eade
Nikozszs <06@yandex.ru>
2026-01-07 00:49:11 +0300

    refactor sectionRussia

---
+++ src/data/neons.tsx
@@ -0,0 +1,12 @@
+import neon from '../assets/neon.svg'
+
+export const neons = [
+    { id: 1, top: 288, left: 208, src: neon },
+    { id: 2, top: 383, left: 188, src: neon },
+    { id: 3, top: 478, left: 263, src: neon },
+    { id: 4, top: 511, left: 58, src: neon },
+    { id: 5, top: 548, left: 358, src: neon },
+    { id: 6, top: 530, left: 378, src: neon },
+    { id: 7, top: 607, left: 558, src: neon },
+    { id: 8, top: 702, left: 1170, src: neon },
+]
