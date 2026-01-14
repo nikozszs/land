886b403e71ddd87858e538ba48eeb85244b22891
Nikozszs <06@yandex.ru>
2026-01-14 11:04:25 +0300

    add header mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,26 @@
+import { useState } from 'react'
 import logo from '../../assets/logo.svg'
 import './Header.css'
 
 export const Header = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    } 
     return (
         <header className='header'>
             <nav className='header__nav'>
                 <div className='header__nav-logo'>
-                    <img src={logo} alt='Логотип'/>
+                    <img src={logo} className='header_logo' alt='Логотип'/>
                     <a className='tel other-color'>+ 7 (800) 600-35-38</a>
+                    <button className='burger-menu' 
+                        onClick={toggleMenu} 
+                        aria-label={isOpen ? 'Закрыть меню' : 'Открыть меню'}
+                        >
+                            <span className={`burger-line ${isOpen ? 'active' : ''}`}></span>
+                            <span className={`burger-line ${isOpen ? 'active' : ''}`}></span>
+                        </button>
                 </div>
                 <div className='header__nav-links'>
                     <a>Технология</a>
