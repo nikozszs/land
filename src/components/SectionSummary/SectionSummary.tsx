694bdaec9f989092e12c65254ea75330d37fdfc2
Nikozszs <06@yandex.ru>
2026-01-16 17:43:51 +0300

    fix section summary

--- src/components/SectionSummary/SectionSummary.tsx
+++ src/components/SectionSummary/SectionSummary.tsx
@@ -5,13 +5,27 @@ import electroTrain from '../../assets/electroTrain.svg'
 export const SectionSummary = () => {
     return (
         <section className='sectionSummary'>
-            <div className='sectionSummary_title'>
-                <h2>не балансируем</h2>
-                <h2>а суммируем</h2>
-            </div>
-            <div className='sectionSummary_img'>
-                <img src={train} />
-                <img src={electroTrain} />
+            <div className='sectionSummary_container'>
+                <div className='desktop-version'>
+                    <div className='sectionSummary_title'>
+                        <h2>не балансируем</h2>
+                        <h2>а суммируем</h2>
+                    </div>
+                    <div className='sectionSummary_img'>
+                        <img src={train} alt='Поезд' />
+                        <img src={electroTrain} alt='Электропоезд' />
+                    </div>
+                </div>
+                <div className='mobile-version'>
+                    <div className='mobile-train'>
+                        <h2>не балансируем</h2>
+                        <img src={train} alt='Поезд' />
+                    </div>
+                    <div className='mobile-electrotrain'>
+                        <h2>а суммируем</h2>
+                        <img src={electroTrain} alt='Электропоезд' />
+                    </div>
+                </div>
             </div>
         </section>
     )
