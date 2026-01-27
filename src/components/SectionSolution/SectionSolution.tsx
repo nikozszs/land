4c9f3479f8f38bbf70af0520f5f4ae7f345bf7c7
Nikozszs <06@yandex.ru>
2026-01-27 14:20:42 +0300

    fix mobile section solution

--- src/components/SectionSolution/SectionSolution.tsx
+++ src/components/SectionSolution/SectionSolution.tsx
@@ -63,7 +63,7 @@ export const SectionSolution = () => {
                 </div>
                 <div className='sectionSolution_block'>
                     <div className='text-container'>
-                        <h3>Индивидуальный подход</h3>
+                        <h3 className='text-break'>Индивидуальный подход</h3>
                         <p className='text'>Мы всегда идем навстречу клиенту, предлагая оптимальное по стоимости решение.</p>
                         <p className='text'>Заполните заявку, и в рабочее время мы за 1 час сделаем расчёт</p>
                     </div>
@@ -74,7 +74,9 @@ export const SectionSolution = () => {
                         <div className='mobile-solution-wrapper'>
                             <div className='sectionSolution_block mobile-slide text-container'>
                                 <h3>{solutionBlocks[currentSlide].title}</h3>
-                                {solutionBlocks[currentSlide].content}
+                                <div>
+                                    {solutionBlocks[currentSlide].content}
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -88,15 +90,15 @@ export const SectionSolution = () => {
                 </div>
 
                 <div className='solution4'>
-                    <h2 className='title'>Попробуйте <br /> 14 дней бесплатно</h2>
-                    <p className='text text-other'>Развивайте свою инфру с <a className='text link'>mrnet</a>. Без лишних затрат и костылей. Высокоскоростной интернет — удобно, эффективно!</p>
+                    <p className='title text-other'>Попробуйте <br /> 14 дней бесплатно</p>
+                    <p className='text-under text-other'>Развивайте свою инфру с <a className='link'>mrnet</a>. Без лишних затрат и костылей. Высокоскоростной интернет — удобно, эффективно!</p>
                 </div>
                 <div className='solution5'>
-                    <img src={solution} alt="Роутер"/>
+                    <img src={solution} className='solution4-img' alt="Роутер"/>
                 </div>
             </div>
             <div className='button-container'>
-                <ButtonGradient text='Начать!'/>
+                <ButtonGradient text='Начать!' disabled={false} type={'button'}/>
             </div>
         </section>
     )
