a6901eed31dd056d6fd697506754f0a21c127e49
Nikozszs <06@yandex.ru>
2026-02-10 17:39:05 +0300

    add sectionPromoTech

---
+++ src/components/ui/SectionPromoTech/SectionPromoTech.tsx
@@ -0,0 +1,16 @@
+import promoTech from '@assets/promoTech.svg'
+import { Button } from '../Button/Button'
+import './SectionPromoTech.css'
+
+export const SectionPromoTech = () => {
+    return (
+        <section className="sectionPromoTech">
+            <img src={promoTech} className='sectionPromoTech-img'/>
+            <div className='block-setting block-sectionPromoBlock'>
+                <h3 className='title-sectionPromoTech'>РЕШЕНИЕ mrnet С ТЕХНОЛОГИЕЙ BONDING ОБЪЕДИНЯЕТ ВСЕ ДОСТУПНЫЕ КАНАЛЫ СВЯЗИ В ЕДИНЫЙ ВИРТУАЛЬНЫЙ КАНАЛ, СУММИРУЕТ СКОРОСТЬ ПЕРЕДАЧИ ДАННЫХ И ПОВЫШАЕТ НАДЕЖНОСТЬ СОЕДИНЕНИЯ</h3>
+                <Button text='оставить заявку'/>
+                <p className='text-under text-sectionPromoTech'>Оставьте заявку и с вами свяжется менеджер и договорится о запуске пилота. <br /> Без необходимости отключения от текущего провайдера и заключения договора. Все цены рассчитываются индивидуально под клиента</p>
+            </div>
+        </section>
+    )
+}
--- src/pages/Technology.tsx
+++ src/pages/Technology.tsx
@@ -1,9 +1,10 @@
 import type { FC } from "react";
+import { SectionPromoTech } from "../components/ui/SectionPromoTech/SectionPromoTech";
 
 export const Technology: FC = () => {
     return(
         <>
-            
+            <SectionPromoTech />
         </>
     )
 }
