45148ff8799395758682a575dc91ddcfbad83746
Nikozszs <06@yandex.ru>
2026-02-11 23:15:56 +0300

    add second-part-scheme

--- src/components/layouts/SectionSсheme/SectionSсheme.tsx
+++ src/components/layouts/SectionSсheme/SectionSсheme.tsx
@@ -1,4 +1,7 @@
 import './SectionScheme.css'
+import sheme1 from '@assets/sheme1.svg'
+import sheme2 from '@assets/sheme2.svg'
+import vector1 from '@assets/vector1.svg'
 
 export const SectionSсheme = () => {
     return (
@@ -21,6 +24,21 @@ export const SectionSсheme = () => {
                     <div className='summary'>
                         <p className='small-text'>суммируем скорость <br /> по каналам</p>
                         <div className='group-border'></div>
+                        <p className='numbers'>20 + 15 + 5 + 50 = 90 Мбит/с</p>
+                    </div>
+                </div>
+                <div className='second-part-scheme'>
+                    <div className='scheme1'>
+                        <img src={sheme1} />
+                        <p className='second-part-text'>клиентские устройства</p>
+                    </div>
+                    <div className='scheme2'>
+                        <p className='second-part-text'>LAN или Wi-Fi</p>
+                        <img className='vector' src={vector1} />
+                    </div>
+                    <div className='scheme2'>
+                        <img className='sheme2-img' src={sheme2} />
+                        <p className='second-part-text'>mrnet мультироутер</p>
                     </div>
                 </div>
             </div>
